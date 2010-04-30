6e4eb261cb6332335b1b8a5506906048b041d4c6
Jonathan <rookieone@gmail.com>
2010-04-29 19:11:42 -0500

    adding code content to NotADesigner

---
+++ NotaDesigner/WpfUtilities/ResourceUtilities/IResourceConvention.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace WpfUtilities.ResourceUtilities
+{
+    public interface IResourceConvention
+    {
+        bool CanBeAppliedTo(string key);
+        void ApplyConvention(Assembly assembly, DictionaryEntry entry);
+    }
+}
---
+++ NotaDesigner/WpfUtilities/ResourceUtilities/ResourceDictionaryConvention.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Windows;
+
+namespace WpfUtilities.ResourceUtilities
+{
+    public class ResourceDictionaryConvention : IResourceConvention
+    {
+        public void ApplyConvention(Assembly assembly, DictionaryEntry entry)
+        {
+            string assemblyName = assembly.GetName().Name;
+            string key = entry.Key as string;
+            key = key.Replace(".baml", ".xaml");
+
+            string uriString = String.Format("pack://application:,,,/{0};Component/{1}", assemblyName, key);
+
+            try
+            {
+                if (!uriString.ToUpper().Contains("VIEW.XAML")
+                    && !uriString.ToUpper().Contains("APP.XAML"))
+                {
+                    var dictionary = new ResourceDictionary
+                                         {
+                                             Source = new Uri(uriString)
+                                         };
+
+                    Application.Current.Resources.MergedDictionaries.Add(dictionary);
+                }
+            }
+            catch
+            {
+                // do nothing. This allows for user controls to 'not' be loaded into the
+                // Resource Dictionary
+            }
+        }
+
+        public bool CanBeAppliedTo(string key)
+        {
+            return key.Contains(".baml");
+        }
+    }
+}
