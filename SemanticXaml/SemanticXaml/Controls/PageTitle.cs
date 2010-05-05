568860180bbe11d815eff8126e8c8aec2c8d8a99
rookieone <rookieone@gmail.com>
2010-05-05 16:32:24 -0500

    added PageTitle control

---
+++ SemanticXaml/SemanticXaml/Controls/PageTitle.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SemanticXaml.Controls
+{
+    public class PageTitle : Control
+    {
+        static PageTitle()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (PageTitle),
+                                                     new FrameworkPropertyMetadata(typeof (PageTitle)));
+
+            TitleProperty = DependencyProperty.Register("Title",
+                                                        typeof (string),
+                                                        typeof (PageTitle));
+        }
+
+        public static readonly DependencyProperty TitleProperty;
+
+        public string Title
+        {
+            get { return (string) GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
---
+++ SemanticXaml/SemanticXaml/ResourceUtilities/IResourceConvention.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace SemanticXaml.ResourceUtilities
+{
+    public interface IResourceConvention
+    {
+        bool CanBeAppliedTo(string key);
+        void ApplyConvention(Assembly assembly, DictionaryEntry entry);
+    }
+}
