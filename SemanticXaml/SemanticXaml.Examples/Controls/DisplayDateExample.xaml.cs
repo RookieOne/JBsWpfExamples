19929aa2ce1edf4384136925f50be1f383972cd1
rookieone <rookieone@gmail.com>
2010-05-05 13:50:20 -0500

    added date controls

---
+++ SemanticXaml/SemanticXaml.Examples/Controls/DisplayDateExample.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace SemanticXaml.Examples.Controls
+{
+    /// <summary>
+    ///   Interaction logic for DisplayDate.xaml
+    /// </summary>
+    public partial class DisplayDateExample : UserControl
+    {
+        public DisplayDateExample()
+        {
+            InitializeComponent();
+
+            date.Date = new DateTime(2010, 5, 5);
+        }
+    }
+}
---
+++ SemanticXaml/SemanticXaml/Controls/DisplayDateRange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SemanticXaml.Controls
+{
+    public class DisplayDateRange : Control
+    {
+        static DisplayDateRange()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (DisplayDateRange),
+                                                     new FrameworkPropertyMetadata(typeof (DisplayDateRange)));
+
+            StartProperty = DependencyProperty.Register("Start",
+                                                        typeof (DateTime?),
+                                                        typeof(DisplayDateRange));
+
+            EndProperty = DependencyProperty.Register("End",
+                                                      typeof (DateTime?),
+                                                      typeof(DisplayDateRange));
+        }
+
+        public static readonly DependencyProperty StartProperty;
+
+        public static readonly DependencyProperty EndProperty;
+
+        public DateTime? Start
+        {
+            get { return (DateTime?) GetValue(StartProperty); }
+            set { SetValue(StartProperty, value); }
+        }
+
+        public DateTime? End
+        {
+            get { return (DateTime?) GetValue(EndProperty); }
+            set { SetValue(EndProperty, value); }
+        }
+    }
+}
